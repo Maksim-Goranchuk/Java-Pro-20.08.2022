890fbe1f6a966819fd66a2f6ee852527cf2620fa
MSI <maksim.goranchuk@gmail.com>
2022-08-20 11:44:29 +0300

    "Hello GitHub " "Hello Hillel " "Хочу научиться программировать java"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello GitHub ");
-        System.out.println("Hello Hillel ");
+        System.out.println("Hello GitHub");
+        System.out.println("Hello Hillel");
+        System.out.println("Хочу научиться программировать java");
+
+        //Хочу научиться программировать java
     }
 }
